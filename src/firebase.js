ad56f6a3d071bc4dfa11a6fbbcf40fbbc977cfae
avinashmohare <mohareavinash80@gmail.com>
2023-12-11 20:46:21 +0530

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyB6lt19vu4m04Ofa3AB0iXhr4BCVLKnS3s",
+    authDomain: "blendedhub-4ca39.firebaseapp.com",
+    projectId: "blendedhub-4ca39",
+    storageBucket: "blendedhub-4ca39.appspot.com",
+    messagingSenderId: "389072964152",
+    appId: "1:389072964152:web:1c04148befa3bbe0afb4cd",
+};
+
+const app = initializeApp(firebaseConfig);
