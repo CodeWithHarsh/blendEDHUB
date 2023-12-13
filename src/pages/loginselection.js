e00efc6bdde9d55e1f26f3bd7c6ae76c0b74806c
harsh.wandhare <harsh.wandhare@gmail.com>
2023-12-13 23:03:39 +0530

    Login Screens

---
+++ src/pages/loginselection.js
@@ -0,0 +1,25 @@
+
+
+import React from "react";
+import { Link } from "react-router-dom";
+import "../../styles/loginselection.css";
+
+const LoginSelection = () => {
+  return (
+    <div className="login-selection">
+      <h1>BlendEdHub</h1>
+      <div>
+        <Link to="/login/student">
+          <button>Login as Student</button>
+        </Link>
+      </div>
+      <div>
+        <Link to="/login/teacher">
+          <button>Login as Teacher</button>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default LoginSelection;
