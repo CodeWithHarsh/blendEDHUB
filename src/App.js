8f5882cc663900129fffb8b216e721c9ab803310
harsh.wandhare <harsh.wandhare@gmail.com>
2023-12-13 23:36:06 +0530

    Login Screen

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import classes from "./App.module.scss";
+
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Login from "./pages/login";
+import LoginSelection from "./pages/loginselection";
+import Register from "./pages/register";
+
 import Dashboard from "./pages/dashboard";
-import MyComponent from "./pages/dashboard";
 function App() {
-    return (
-        <div className={classes.App}>
-            <Dashboard />
-        </div>
-    );
+  return (
+    <div className="app">
+      <Router>
+        <Routes>
+          <Route path="/" element={<LoginSelection />} />
+          <Route path="/login/student" element={<Login userType="student" />} />
+          <Route exact path="/register" element={<Register />} />
+          <Route exact path="/dashboard" element={<Dashboard />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
